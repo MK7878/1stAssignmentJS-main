7bee62e1fbe38dc3962b5b510da609e00bc2a6e0
MK7878 <89302582+MK7878@users.noreply.github.com>
2021-09-03 16:23:19 +0600

    1stAssignmentJS-main

---
+++ 1stAssignmentJS-main/assignment.js
@@ -0,0 +1,126 @@
+// // ১. Variable কি ?
+
+Ex. ভ্যারিয়েবল হচ্ছে এমন একটা জিনিস যেখানে আমারা যেকোনো কিছু সংগ্রহ
+করতে পারি, যেকোনো কিছু স্টোর করে রাখতে পারি। যেকোনো কিছু পরিবর্তন করতে পারি
+সেটা হতে পাতে ভ্যালু বা অনন্য কিছু।
+
+// // ২. Variable কিভাবে লিখতে হয় ?
+
+Ex. ভ্যারিয়েবল সাধারণত সংক্ষিপ্ত আকারে লিখতে হয়। তাই ভ্যারিয়েবল কে  জাভাস্ক্রিপ্ট এ var লিখতে হয়।
+
+ যেমনঃ var number = 50;
+
+// // ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+ Ex. স্ট্রিং টাইপ ভ্যারিয়েবল কোন টেক্সট কে বুঝানো হয়।
+
+ যেমনঃ 
+
+    var strType = "My name is Mustakim Khan";
+    এটার আউটপুত জানার জন্য লিখতে হবে,
+    console.log(strType);
+
+    এখন এটা স্ট্রিং টাইপ ভ্যারিয়েবল কিনা তা জানার জন্য লিখতে হবে ,
+    console.log(typeof strType);
+
+
+// // ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+
+ Ex. নাম্বার টাইপ ভ্যারিয়েবল হচ্ছে যে ভ্যারিয়েবল দ্বারা সংখ্যাকে বুঝানো হয়। 
+ 
+ যেমনঃ 
+ 
+    var numberType = 80;
+    এটার আউটপুত জানার জন্য লিখতে হবে,
+    console.log(numberType);
+
+    এখন এটা নাম্বার টাইপ ভ্যারিয়েবল কিনা তা জানার জন্য লিখতে হবে ,
+    console.log(typeof numberType);
+
+// // ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+
+ Ex. Boolan type variable সাধারণত true or false এর মদ্ধে হয়ে থাকে। সেই ভালু টা সত্য নাকি মিত্থা তা এই ভ্যারিয়েবল এর মাদ্ধমে বোঝা যায়।
+
+যেমনঃ
+
+    var boolType = true;
+    এটার আউটপুত জানার জন্য লিখতে হবে,
+    console.log(boolType);
+
+    এখন এটা বুলিয়ান টাইপ ভ্যারিয়েবল কিনা তা জানার জন্য লিখতে হবে ,
+    console.log(typeof boolType);
+    
+
+    var boolType2 = false;
+    এটার আউটপুত জানার জন্য লিখতে হবে,
+    console.log(boolType2);
+
+    এখন এটা বুলিয়ান টাইপ ভ্যারিয়েবল কিনা তা জানার জন্য লিখতে হবে ,
+    console.log(typeof boolType2);
+
+// // ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+ Ex. var simpleText = "My name is Mustakim Khan";
+    এই লেখা uppercase অর্থাৎ সবগুলা টেক্সট কে বড় করতে হলে লিখতে হবে,  
+    console.log(simpleText.toUpperCase());
+
+     var simpleText2 = "My name is Mustakim Khan";
+    এই লেখা lowercase অর্থাৎ সবগুলা টেক্সট কে ছোট করতে হলে লিখতে হবে,  
+    console.log(simpleText2.toLowerCase());
+
+// // ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+   Ex.  জাভাস্ক্রিপ্ট এর মোট ওপারেটর পাচটি।
+    যেমনঃ ১। যোগ
+    ২। বিয়োগ
+    ৩। গুন
+    ৪। ভাগ
+    ৫। ভাগশেষ। 
+
+// // ৮. Math.abs() এর ব্যাবহার লিখুন । 
+
+   Ex. var number1 = -30;
+   console.log(Math.abs(number1));  
+
+// // ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+
+   Ex. var number1 = 0.4;
+   console.log(Math.ceil(number1));
+
+   Ex. var number2 = 0.8;
+   console.log(Math.ceil(number2));
+
+   Ex. var number3 = 10.5;
+   console.log(Math.ceil(number3));
+
+// // ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+
+   Ex. var number1 = 30.1;
+   console.log(Math.floor(number1));
+
+   Ex. var number2 = 0.8;
+   console.log(Math.floor(number2));
+
+   Ex. var number3 = 80.5;
+   console.log(Math.floor(number3));
+
+// // ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+   Ex. var number1 = 0.5;
+   console.log(Math.round(number1));
+
+   Ex. var number2 = 0.2;
+   console.log(Math.round(number2));
+
+   Ex. var number3 = 500.2888;
+   console.log(Math.round(number3));
+
+   Ex. var number4 = 200.8100;
+   console.log(Math.round(number4));
+
+// // ১২. Math.random() এর ব্যাবহার লিখুন ।
+
+   Ex. var number1 = 0.44;
+   console.log(Math.random(number1)); 
+   
+// // asignment.js - GitHub Link 
